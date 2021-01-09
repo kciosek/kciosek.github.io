ddbff79cacd1cb34a43e339e1f3e707c6b2c1f6a
Kamil Ciosek <kamil.ciosek@posteo.net>
2021-01-09 19:42:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+ var toggle = function(id) 
+  {
+  	var mydiv = document.getElementById(id);  	
+  	if (mydiv.style.display === 'block')
+    	mydiv.style.display = 'none';
+  	else
+    	mydiv.style.display = 'block'
+  }
